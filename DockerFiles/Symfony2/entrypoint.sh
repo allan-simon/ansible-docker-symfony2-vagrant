31ef4f5b0238dd0d43f938835fc37c15b450c9c7
Allan Simon <simona@gobeta.com.cn>
2015-04-14 22:47:38 +0800

    now we run migrations in the entry point and we make sure to run in non-interactive mode

--- DockerFiles/Symfony2/entrypoint.sh
+++ DockerFiles/Symfony2/entrypoint.sh
@@ -13,7 +13,13 @@ if [ -z "$1" ];
     # if you're in china and that for some reason
     # the docker build has failed to download composer.phar...
     if ! which composer; then
-        curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
+        if [ -f composer.phar ]; then
+            cp composer.phar /usr/local/bin/composer
+            chmod +x /usr/local/bin/composer
+        else
+            curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+        fi
     fi
     # variable provided by ansible when launching the docker container
     mkdir ~/.composer
@@ -26,11 +32,12 @@ if [ -z "$1" ];
 EOS
     # we make sure to start fresh
     rm app/config/parameters.yml
-    composer install
+    composer install --no-interaction
     rm -rf app/cache/*
     php app/console assets:install --symlink web/
     php app/console c:c
     php app/console c:w
+    php app/console doctrine:migrations:migrate --no-interaction
     # not pretty ...
     chmod -R 777 app/logs
     chmod -R 777 app/cache
